1a9e022d98480454ffbc4eed7355afed756edf89
Sathya Krishnan <ts.sathyakrishnan2002@gmail.com>
2023-10-21 07:52:48 +0530

    chatbot deployment using flask

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request, render_template, jsonify
+from flask_cors import CORS
 from saige import SAIGE
+import socket
 
 bot = SAIGE()
 
+
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/qa", methods=["POST"])
-def qa():
-  question = request.json["question"]
-  answer = bot.query(query=question)
 
-  return jsonify({"answer": answer})
 
-if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5000)
+
+@app.post('/saige')
+def get_answer():
+    text = request.get_json().get("message")
+    response = bot.query(query=text)
+    # response = "Hi"
+    message = {"answer" : response}
+    return jsonify(message)
+
+if __name__ == '__main__':
+   app.run(host='LOCALHOST', port="PORT", debug=False, threaded=False)
