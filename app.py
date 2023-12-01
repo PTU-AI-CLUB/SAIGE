e1adb68c3a5398d5e0bd959428d7fd91576ad39b
Dwaipayan De <103761481+dwaipayandex@users.noreply.github.com>
2023-12-01 16:03:14 +0530

    Update app.py to Notify users about direct API Access

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template, jsonify
+from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
 from saige import SAIGE
 
@@ -7,13 +7,16 @@ bot = SAIGE()
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 @app.post('/saige')
 def get_answer():
     text = request.get_json().get("message")
     response = bot.query(query=text)
-    # response = "Hi"
-    message = {"answer" : response}
+    message = {"answer": response}
     return jsonify(message)
 
 if __name__ == '__main__':
-   app.run(host='LOCALHOST', port="PORT", debug=False, threaded=False)
+   app.run(host='0.0.0.0', port="5000", debug=False, threaded=False)
